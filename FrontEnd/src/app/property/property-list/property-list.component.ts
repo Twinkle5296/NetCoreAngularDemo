d70666a436289712b8ebee3e8ef1a3092f08e6e8
Twinkle5296 <52199659+Twinkle5296@users.noreply.github.com>
2023-05-08 15:37:27 -0400

    Created Property List

--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -6,6 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
+  properties: Array<any>=[
+    {
+    "ID":1,
+    "Name":"Birla House",
+    "Type":"House",
+    "Price":12000
+    },
+    {
+      "ID":2,
+      "Name":"Erose Villa",
+      "Type":"Villa",
+      "Price":12500
+    },
+    {
+      "ID":3,
+      "Name":"Mark List",
+      "Type":"Flat",
+      "Price":13000
+    },
+    {
+      "ID":4,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":15000
+    },
+    {
+      "ID":5,
+      "Name":"Macro Home",
+      "Type":"House",
+      "Price":14500
+    },
+    {
+      "ID":6,
+      "Name":"Pearl White House",
+      "Type":"House",
+      "Price":12600
+    }
+
+
+]
 
   constructor() { }
 
