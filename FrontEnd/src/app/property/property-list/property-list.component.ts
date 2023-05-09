0d32ab44b8cb8cd598dbfa9d03584e167f038200
Twinkle5296 <52199659+Twinkle5296@users.noreply.github.com>
2023-05-09 12:26:22 -0400

    sing Service and HTTP calls in Angular 9

--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+
 
 @Component({
   selector: 'app-property-list',
@@ -6,50 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-  properties: Array<any>=[
-    {
-    "ID":1,
-    "Name":"Birla House",
-    "Type":"House",
-    "Price":12000
-    },
-    {
-      "ID":2,
-      "Name":"Erose Villa",
-      "Type":"Villa",
-      "Price":12500
-    },
-    {
-      "ID":3,
-      "Name":"Mark List",
-      "Type":"Flat",
-      "Price":13000
-    },
-    {
-      "ID":4,
-      "Name":"Birla House",
-      "Type":"House",
-      "Price":15000
-    },
-    {
-      "ID":5,
-      "Name":"Macro Home",
-      "Type":"House",
-      "Price":14500
-    },
-    {
-      "ID":6,
-      "Name":"Pearl White House",
-      "Type":"House",
-      "Price":12600
-    }
 
+  properties: any;
 
-]
+  constructor(private housingService:HousingService) {
+   
+   }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit():void {
+    this.housingService.getAllProperties().subscribe(
+      data=>{
+           this.properties=data;
+          console.log(data);
+          }
+    )
+    
   }
 
 }
